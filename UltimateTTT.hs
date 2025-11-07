23defae43021c2ced919b5823d08de7794e63b5b
Jesse Gamblin <jgamblin@trinity.edu>
2025-11-06 21:45:37 -0600

    Data Type Shit

--- UltimateTTT.hs
+++ UltimateTTT.hs
@@ -1,1 +1,22 @@
+data Player = PlayerX | PlayerO deriving (Show, Eq)
+-- helper function to change player 
+
+data Winner = Win Player | Draw | OnGoing deriving (Eq, Show)
+
+data Square = X | O | Empty deriving (Eq, Show)
+
+type GameBoard = [[Sqaure]]
+
+type smallBox = [[Sqaure]]
+
+type bigBox = [[smallBox]]
+
+type location = (int, int) 
+
+data prevMove = location
+--helper function to change prev move and keep track like the player helper function
+
+
+
+
 
