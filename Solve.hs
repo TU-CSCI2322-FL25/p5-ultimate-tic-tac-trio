6aa1ef94e70c265cebc2ea5d55de539a20de2b21
Haydy Castillo <hcastill@trinity.edu>
2025-12-08 11:46:52 -0600

    17

--- Solve.hs
+++ Solve.hs
@@ -48,3 +48,53 @@ bestMove g@(board, player, premove)
         winningMoves = [x | x <- moves, outcome x == Win player]
         drawMoves = [x | x <- moves, outcome x == Draw]
 -- end story 10
+
+rateGame :: Game -> Rating
+rateGame (board,_,_) =
+    case checkWinner (board, X, (0,0)) of
+        Just (Win X) -> 10  --x wins and gets highest score of 10
+        Just (Win O) -> -10 --o
+        Just Draw -> 0
+        Nothing -> evaluateBoard board
+
+evaluateBoard :: GameBoard -> Int
+evaluateBoard board =
+    scoreSmallBoards flatBoards + sum (map scoreInSmallboard flatBoards)
+    where
+        flatBoards = concat board --3*3 into list of 9
+
+--if same player wins adjacent boards +1 point
+scoreSmallBoards :: [SmallBoard] -> Int
+scoreSmallBoards (a:b:rest) =
+    scorePair a b + scoreSmallBoards (b:rest)
+  where
+    scorePair (Finished (Win X)) (Finished (Win X)) =  1
+    scorePair (Finished (Win O)) (Finished (Win O)) = -1
+    scorePair _ _ = 0
+scoreSmallBoards _ = 0
+
+--boxes in small board
+scoreInSmallboard :: SmallBoard -> Int
+scoreInSmallboard (Finished _) = 0   -- finished boards dont contribute
+
+scoreInSmallboard (UnFinished [[a,b,c],[d,e,f],[g,h,i]]) =
+    sum $ map scoreLine wins
+  where
+    -- All possible lines in the smallboard: rows, columns, diagonals
+    wins = [[a,b,c],[d,e,f],[g,h,i],   -- rows
+            [a,d,g],[b,e,h],[c,f,i],   -- columns
+            [a,e,i],[c,e,g]]           -- diagonals
+
+    -- Score a line: +1 if X has 2 and one empty, -1 if O has 2 and one empty
+    scoreLine :: [Square] -> Int
+    scoreLine line
+      | length (filter (== Full X) line) == 2 && length (filter (== Empty) line) == 1 = 1
+      | length (filter (== Full O) line) == 2 && length (filter (== Empty) line) == 1 = -1
+      | otherwise = 0
+
+--win 10
+--loss -10
+
+--1 point for 2 smallboard wins in a row
+-- .5 point for 2 boxes in a row per smallboard that has not been won
+--end story 17
